53c9f4872b332ebccd1050f9cbe5e0c9aa19a59c
Huseen <136820327+hmunye@users.noreply.github.com>
2025-09-25 18:06:44 -0400

    setup clap and subcommands for encoding, decoding, and removing messages within PNG files

--- README.md
+++ README.md
@@ -11,6 +11,21 @@ cd pngme
 
 ```bash
 cargo b --release
+./target/release/pngme help
+```
+
+```
+Usage: pngme <command>
+
+Commands:
+  encode  Encodes a message into the PNG file given its chunk type
+  decode  Decodes a message from the PNG file given the chunk type
+  remove  Removes a message from the PNG file given the chunk type
+  help    Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,83 @@
 #![deny(missing_docs)]
 #![warn(missing_debug_implementations)]
 #![warn(rust_2018_idioms)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
 mod chunk;
 mod chunk_type;
 mod png;
 
-use anyhow::Result;
+use std::path::PathBuf;
 
-fn main() -> Result<()> {
-    todo!()
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "pngme")]
+#[command(version, about = None, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+#[command(subcommand_value_name = "command")]
+enum Commands {
+    /// Encodes a message into the PNG file given its chunk type.
+    #[command(arg_required_else_help = true)]
+    Encode {
+        #[command(flatten)]
+        opts: CommandOpts,
+        #[arg(value_name = "message")]
+        message: String,
+    },
+    /// Decodes a message from the PNG file given the chunk type.
+    #[command(arg_required_else_help = true)]
+    Decode {
+        #[command(flatten)]
+        opts: CommandOpts,
+    },
+    /// Removes a message from the PNG file given the chunk type.
+    #[command(arg_required_else_help = true)]
+    Remove {
+        #[command(flatten)]
+        opts: CommandOpts,
+    },
+}
+
+#[derive(Args, Debug)]
+struct CommandOpts {
+    #[arg(value_name = "file_path")]
+    file_path: PathBuf,
+    #[arg(value_name = "chunk_type")]
+    chunk_type: String,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Encode { opts, message } => {
+            println!(
+                "encoding message '{}' as type '{}' in file {}",
+                message,
+                opts.chunk_type,
+                opts.file_path.display()
+            );
+        }
+        Commands::Decode { opts } => {
+            println!(
+                "decoding type '{}' from file {}",
+                opts.chunk_type,
+                opts.file_path.display()
+            );
+        }
+        Commands::Remove { opts } => {
+            println!(
+                "removing type '{}' from file {}",
+                opts.chunk_type,
+                opts.file_path.display()
+            );
+        }
+    }
 }
